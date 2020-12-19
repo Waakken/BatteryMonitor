2d2b3f7806ad85b283d7bee126fd1491b0b28c67
Jani Kuutvuori <janikuutvuori@hotmail.com>
2020-12-19 19:26:44 +0200

    Do some refactoring

--- app/src/main/java/com/example/batterymonitor/MainActivity.java
+++ app/src/main/java/com/example/batterymonitor/MainActivity.java
@@ -18,7 +18,6 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     public static final String extra_msg = "hello";
-    private static final String TAG = MainActivity.class.getSimpleName();
     private int prevCharge = 0;
     private int events = 0;
 
@@ -35,10 +34,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             // this is where we deal with the data sent from the battery.
-            int charge = intent.getIntExtra(String.valueOf(BatteryManager.EXTRA_LEVEL), -1);
+            int charge = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
             //if (charge != prevCharge) {
-                Log.d("batteryStatusReceiver", "Battery level: " + charge);
-                handleBatteryStatus(intent);
+            Log.d("batteryStatusReceiver", "Battery level: " + charge);
+            handleBatteryStatus(intent);
             //}
             prevCharge = charge;
             events++;
@@ -49,32 +48,21 @@ public class MainActivity extends AppCompatActivity {
     };
 
     public void clearMessages(View view) {
-        //Log.d("clearMessages", "hello");
         TextView messageField = findViewById(R.id.messageField);
         messageField.setText("");
     }
 
     public void sendMessage(View view) {
         writeBatteryCharge(prevCharge, "(button)");
-        //writeBatteryLevel(batteryStatus);
-        /*
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editTextTextPersonName2);
-        String msg = editText.getText().toString();
-        intent.putExtra(extra_msg, msg);
-        startActivity(intent);
-         */
     }
 
     private void handleBatteryStatus(Intent batteryStatus) {
-        int charge = batteryStatus.getIntExtra(String.valueOf(BatteryManager.EXTRA_LEVEL), -1);
+        int charge = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
         writeBatteryCharge(charge, "(event)");
     }
 
     private void writeBatteryCharge(int charge, String msg) {
-        //Log.d(TAG, "Battery level: " + charge);
         String curDate = getCurrentTime();
-        //Log.d(TAG, "Date " + curDate);
 
         TextView messageField = findViewById(R.id.messageField);
         messageField.append(curDate + " Battery level: " + charge + " " + msg + "\n");
@@ -83,17 +71,14 @@ public class MainActivity extends AppCompatActivity {
     private String getCurrentTime() {
         Calendar curCalendar = Calendar.getInstance();
         curCalendar.get(Calendar.DAY_OF_WEEK);
-        int seconds = curCalendar.get(Calendar.SECOND);
-        Date curDate = curCalendar.getTime();
 
         String minuteStr = getZeroPaddedNumber(curCalendar.get(Calendar.MINUTE));
         String hourStr = getZeroPaddedNumber(curCalendar.get(Calendar.HOUR_OF_DAY));
         String secondStr = getZeroPaddedNumber(curCalendar.get(Calendar.SECOND));
 
-        String retVal = hourStr + ":" +
+        return hourStr + ":" +
                 minuteStr + ":" +
                 secondStr;
-        return retVal;
     }
 
     private String getZeroPaddedNumber(int number) {
